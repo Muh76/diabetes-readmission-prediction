1a6d14c76117656c47fdbf7c40070de8c6f4e376
MohammadJavadAghababaieBeni <mj.babaie@gmail.com>
2025-08-19 21:22:08 +0100

    🧪 MINIMAL TEST: Created simplest possible test to isolate CI issue

---
+++ tests/test_minimal.py
@@ -0,0 +1,13 @@
+"""
+Minimal test to isolate CI issues
+"""
+
+
+def test_minimal():
+    """Absolute minimal test"""
+    assert True
+
+
+def test_basic_math():
+    """Basic math test"""
+    assert 1 + 1 == 2
